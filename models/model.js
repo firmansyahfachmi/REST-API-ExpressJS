48bc130fe86ef43ea1ee561a191b385bedf4dab3
M Fachmi Firmansyah <firmansyahfachmi5@gmail.com>
2019-08-20 11:42:13 +0700

    add folder

--- models/model.js
+++ models/model.js
@@ -17,8 +17,8 @@ module.exports = {
     getProductBy: (data) => {
         return new Promise((resolve, reject) => {
             const page = (data.page * data.limit) - data.limit;
-                db.query(`SELECT * FROM product WHERE type LIKE '%${data.type}%' LIMIT ${page},${data.limit}`, (err, response) => {
-            
+            db.query(`SELECT * FROM product WHERE type LIKE '%${data.type}%' LIMIT ${page},${data.limit}`, (err, response) => {
+
                 if (!err) {
                     resolve(response);
                 } else {
@@ -45,7 +45,7 @@ module.exports = {
 
     updateProduct: (data, id) => {
         return new Promise((resolve, reject) => {
-            db.query(`UPDATE product SET ? WHERE id='${id}'`,[data], (err, response) => {
+            db.query(`UPDATE product SET ? WHERE id='${id}'`, [data], (err, response) => {
 
                 if (!err) {
                     resolve(response);
